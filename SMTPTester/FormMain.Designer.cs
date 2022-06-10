b3d1b6655a61e678947fb65bc4e50009f1aa374b
Joao Leitao <joaoleitao87@gmail.com>
2022-06-10 10:42:17 +0100

    feat: Initial version

---
+++ SMTPTester/FormMain.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SMTPTester
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.chkUseSenderAsUsername = new System.Windows.Forms.CheckBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.chkUseSSL = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(197, 280);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(12, 15);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(32, 13);
+            this.lblHost.TabIndex = 1;
+            this.lblHost.Text = "Host:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(12, 41);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(29, 13);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(12, 90);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(33, 13);
+            this.lblFrom.TabIndex = 3;
+            this.lblFrom.Text = "From:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(12, 139);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 5;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 165);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(12, 191);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(23, 13);
+            this.lblTo.TabIndex = 7;
+            this.lblTo.Text = "To:";
+            // 
+            // chkUseSenderAsUsername
+            // 
+            this.chkUseSenderAsUsername.AutoSize = true;
+            this.chkUseSenderAsUsername.Location = new System.Drawing.Point(15, 113);
+            this.chkUseSenderAsUsername.Name = "chkUseSenderAsUsername";
+            this.chkUseSenderAsUsername.Size = new System.Drawing.Size(143, 17);
+            this.chkUseSenderAsUsername.TabIndex = 4;
+            this.chkUseSenderAsUsername.Text = "Use sender as username";
+            this.chkUseSenderAsUsername.UseVisualStyleBackColor = true;
+            this.chkUseSenderAsUsername.CheckedChanged += new System.EventHandler(this.chkUseSenderAsUsername_CheckedChanged);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(76, 136);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 20);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(76, 162);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(196, 20);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFrom.Location = new System.Drawing.Point(76, 87);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(196, 20);
+            this.txtFrom.TabIndex = 3;
+            this.txtFrom.TextChanged += new System.EventHandler(this.txtFrom_TextChanged);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTo.Location = new System.Drawing.Point(76, 188);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTo.Size = new System.Drawing.Size(196, 83);
+            this.txtTo.TabIndex = 7;
+            this.txtTo.TextChanged += new System.EventHandler(this.txtTo_TextChanged);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHost.Location = new System.Drawing.Point(76, 12);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(196, 20);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.TextChanged += new System.EventHandler(this.txtHost_TextChanged);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPort.Location = new System.Drawing.Point(76, 38);
+            this.txtPort.MaxLength = 5;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(196, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.TextChanged += new System.EventHandler(this.txtPort_TextChanged);
+            // 
+            // chkUseSSL
+            // 
+            this.chkUseSSL.AutoSize = true;
+            this.chkUseSSL.Location = new System.Drawing.Point(15, 64);
+            this.chkUseSSL.Name = "chkUseSSL";
+            this.chkUseSSL.Size = new System.Drawing.Size(68, 17);
+            this.chkUseSSL.TabIndex = 2;
+            this.chkUseSSL.Text = "Use SSL";
+            this.chkUseSSL.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 315);
+            this.Controls.Add(this.chkUseSSL);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.chkUseSenderAsUsername);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "SMTP Tester";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.CheckBox chkUseSenderAsUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.CheckBox chkUseSSL;
+    }
+}
+
---
+++ SMTPTester/FormMain.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMTPTester
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+#if DEBUG
+            txtHost.Text = "smtp.gmail.com";
+            txtPort.Text = "587"; // could also be 465
+            chkUseSSL.Checked = true;
+            chkUseSenderAsUsername.Checked = false;
+#endif
+
+            RefreshControls();
+        }
+
+        private void txtHost_TextChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void txtPort_TextChanged(object sender, EventArgs e)
+        {
+            txtPort.Text = FilterNumeric(txtPort.Text);
+            RefreshControls();
+        }
+
+        private void chkUseSenderAsUsername_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void txtFrom_TextChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void txtTo_TextChanged(object sender, EventArgs e)
+        {
+            RefreshControls();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+
+            try
+            {
+                SendEmail(Host, Port, Username, Password, Sender, Receivers);
+
+                MessageBox.Show("Mail sent");
+            }
+            catch (Exception ex)
+            {
+                string tab = "";
+                string details = "Message: " + ex.Message;
+                details += Environment.NewLine + "Stack: " + ex.StackTrace;
+                while ((ex = ex.InnerException) != null)
+                {
+                    tab += "\t";
+                    details += Environment.NewLine + tab + "Message: " + ex.Message;
+                    details += Environment.NewLine + tab + "Stack: " + ex.StackTrace.Replace(Environment.NewLine, Environment.NewLine + tab);
+                }
+                MessageBox.Show(details);
+            }
+
+            btnSend.Enabled = true;
+        }
+
+        private void RefreshControls()
+        {
+            txtUsername.Enabled = !chkUseSenderAsUsername.Checked;
+
+            btnSend.Enabled = !String.IsNullOrEmpty(txtHost.Text)
+                           && !String.IsNullOrEmpty(txtPort.Text)
+                           && (chkUseSenderAsUsername.Checked || !String.IsNullOrEmpty(txtUsername.Text))
+                           && !String.IsNullOrEmpty(txtPassword.Text)
+                           && !String.IsNullOrEmpty(txtFrom.Text)
+                           && !String.IsNullOrEmpty(txtTo.Text);
+        }
+
+        private string FilterNumeric(string text)
+        {
+            string filtered = "";
+            foreach (char c in text)
+                if (c >= '0' && c <= '9')
+                    filtered += c;
+
+            return filtered;
+        }
+
+        private void SendEmail(string host, int port, string username, string password, string from, List<string> to)
+        {
+            int attempt = 0;
+
+Try_Again:
+            attempt++;
+            try
+            {
+                //Set up SMTP client
+                SmtpClient client = new SmtpClient();
+                client.Host = host;
+                client.Port = port;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.EnableSsl = chkUseSSL.Checked;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(username, password);
+
+                //Set up the email message
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(from);
+                foreach (string receiver in to)
+                    message.To.Add(new MailAddress(receiver));
+                message.Subject = "SMTP Test";
+                message.IsBodyHtml = true;
+                message.Body = "This is a test e-mail from a DotNet SMTP client (generated at " + DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss UTC") + ")";
+
+                //Attempt to send the email
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                if (ex is SmtpException)
+                {
+                    if ((attempt < 3) && (ex.Message.Contains("5.7.1"))) goto Try_Again;
+                }
+
+                throw ex;
+            }
+        }
+
+        private string Host
+        {
+            get
+            {
+                return txtHost.Text;
+            }
+        }
+
+        private int Port
+        {
+            get
+            {
+                return int.Parse(txtPort.Text);
+            }
+        }
+
+        private string Sender
+        {
+            get
+            {
+                return txtFrom.Text;
+            }
+        }
+
+        private bool UseSenderAsUsername
+        {
+            get
+            {
+                return chkUseSenderAsUsername.Checked;
+            }
+        }
+
+        private string Username
+        {
+            get
+            {
+                if (UseSenderAsUsername)
+                    return Sender;
+                else
+                    return txtUsername.Text;
+            }
+        }
+
+        private string Password
+        {
+            get
+            {
+                return txtPassword.Text;
+            }
+        }
+
+        private List<string> Receivers
+        {
+            get
+            {
+                List<string> recs = new List<string>();
+
+                foreach (string line in txtTo.Lines)
+                    if (!String.IsNullOrEmpty(line))
+                        recs.Add(line);
+
+                return recs;
+            }
+        }
+    }
+}
